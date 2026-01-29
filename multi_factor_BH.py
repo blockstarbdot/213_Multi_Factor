7cfa386938ca1d8afd7e1f1a51c150ca720b31c0
blockstarbdot <Brendan.hicks@stu.vertexacademies.org>
2026-01-29 19:12:35 +0000

    changed password

--- multi_factor_BH.py
+++ multi_factor_BH.py
@@ -1,9 +1,14 @@
+
 # a213_multi_factor.py
 import multifactorcli as mfc
 
 # create a multi-factor interface to a restricted app
-my_auth = mfc.MultiFactorAuth()
+my_auth = mfc.MultiFactorAuth() # This is a class object
 
+#set the username and password
+username = "banana"
+password = "apple"
+my_auth.set_authetication(username, password)
 # set the users authentication information
 question = "What is your favorite color?"
 answer = "purple"
